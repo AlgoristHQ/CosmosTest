b79a509e6f975ec42c062cee8dd399005799a023
The Algorist <TheEngineer@Algorist.io>
2021-08-16 00:49:46 -0500

    Added MongoDb library for connecting to CosmosDb

--- CosmosTest/CosmosRepo.cs
+++ CosmosTest/CosmosRepo.cs
@@ -1,6 +1,7 @@
 ﻿using CosmosTest.CosmosLib;
 using CosmosTest.DocumentDb;
 using CosmosTest.EFCore;
+using CosmosTest.MongoDb;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace CosmosTest
         private CosmosLibToCosmos _cosmosLibToCosmos = new CosmosLibToCosmos();
         private DocumentClientToCosmos _documentClientToCosmos = new DocumentClientToCosmos();
         private EFCoreToCosmos _efCoreToCosmos = new EFCoreToCosmos();
+        MongoDbToCosmos _mongoDbToCosmos = new MongoDbToCosmos();
 
         public List<DocumentDb.CosmosDocumentType> UseDocumentClient()
         {
@@ -25,6 +27,11 @@ namespace CosmosTest
         public List<EFCore.CosmosDocumentType> UseEFCore()
         {
             return _efCoreToCosmos.GetAll();
+        }
+
+        public List<MongoDb.CosmosDocumentType> UseMongoDb()
+        {
+            return _mongoDbToCosmos.GetAll();
         }
     }
 }
